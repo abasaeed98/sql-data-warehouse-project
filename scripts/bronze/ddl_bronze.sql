a4dbb6be4166a213250fb24077aced1a79b69084
abasaeed98 <153679594+abasaeed98@users.noreply.github.com>
2025-10-29 01:49:55 +0300

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -46,3 +46,10 @@ CREATE TABLE bronze.erp_loc_a101 (
 cid VARCHAR(50),
 cntry VARCHAR(50)
 );
+
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+id	VARCHAR(50),
+cat	VARCHAR(50),
+subcat VARCHAR(50),
+maintenance VARCHAR(50)
+);
